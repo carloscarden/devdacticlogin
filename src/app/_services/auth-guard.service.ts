68ab456da88bcaa295b707799258158c6deed9bb
Carlos <cardencarlos@abc.gob.ar>
2019-04-11 14:22:14 -0300

    arreglando campitos boludos

--- src/app/_services/auth-guard.service.ts
+++ src/app/_services/auth-guard.service.ts
@@ -12,13 +12,17 @@ export class AuthGuardService implements CanActivate{
     public authenticationService: AuthenticationService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    console.log("entra");
     const currentUser = this.authenticationService.currentUserValue;
+    
     if (currentUser) {
         // logged in so return true
+        console.log("autorizado");
         return true;
     }
 
     // not logged in so redirect to login page with the return url
+    console.log("no autorizado");
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
